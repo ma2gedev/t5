015d706d069dd4c8e41cbdd60d56d64d9f9868fe
Takayuki Matsubara <takayuki.1229@gmail.com>
2013-07-18 15:20:20 +0900

    added proxy support code with comment out

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ Capybara.configure do |c|
   c.app = 'avoid "mechanize requires a rack application" error'
   c.default_driver = :mechanize
   c.app_host = 'http://replace.your.app.host'
+  # if you use proxy set the following
+  # Capybara.current_session.driver.browser.agent.set_proxy('proxy.host', 'proxy.port')
 end
 
 RSpec.configure do |c|
